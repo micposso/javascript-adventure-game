3ec5ad9f384d771662292e127bd3fce2bc0c8a68
michael posso <micposso@gmail.com>
2021-12-10 15:38:45 -0500

    added objects with text for the game

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("script is loading");
+
+let firstStage = {
+    id: 1,
+    story: "Juan got lost in the forest, please help juan find his way out now!",
+    options: ["start heading north", "start heading south", "find a path", "follow the river"]
+}
+
+let secondStage = {
+
+}
+
+let thirdSatege = {
+
+}
+
+let fourthStage = {
+
+}
